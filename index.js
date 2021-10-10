100798bdf990faf35179f2f03c1cb16946ba489e
marnix <marnix@hagehoeve.com>
2021-10-10 16:23:04 +0200

    fix: added check if guild is in list

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const saveScore = () =>
 setInterval(() => saveScore(), 10 * 1000)
 
 client.on("message", (message) => {
+  if (typeof scores[message.guild.id] !== "number") scores[message.guild] = 0
   if (message.content.startsWith(`${prefix}help`)) {
     message.channel.send(`__Help__
     
